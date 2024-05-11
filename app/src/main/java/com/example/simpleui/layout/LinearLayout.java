96c35f3f53887f7656677c41398c9b830e803770
trandtrieu <114125026+trandtrieu@users.noreply.github.com>
2024-05-11 22:34:48 +0700

    Update

---
+++ app/src/main/java/com/example/simpleui/layout/LinearLayout.java
@@ -0,0 +1,25 @@
+package com.example.simpleui.layout;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.simpleui.R;
+
+public class LinearLayout extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.linear_layout);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.linear), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
